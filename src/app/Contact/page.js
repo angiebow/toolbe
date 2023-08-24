20866ecfb36c8b9747d51d9fb7dbe302a6b7d4a4
Pelangi Masita <pelangimasita18@gmail.com>
2023-08-24 22:27:33 +0700

    Added the Contact Us page

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,28 +1,50 @@
-import Footer from "@/components/footer/Footer"
+"use client"
+// import Footer from "../components/footer/Footer"
 import React from 'react'
 import localFont from "next/font/local";
 import Whitebg from "@/components/whitebg/Whitebg";
+import contactApi from '../apicontact/contact'; 
 
 const lun = localFont({ src: '/lunema-regular.ttf' });
 const me = localFont({ src: '/monumentextendedreg.otf' });
 
 const Contact = () => {
+  const handleContact = async (event) => {
+    event.preventDefault();
+    const data = {
+      fullname: event.target.fullname.value,
+      lastname: event.target.lastname.value,
+      email: event.target.email.value,
+      phone_number: event.target.phone_number.value,
+      topic: event.target.topic.value,
+      message: event.target.message.value,
+    };
+    console.log(data);
+
+    try {
+      const response = await contactApi(data); 
+      console.log('Contact successful:', response);
+    } catch (error) {
+      console.error('Contact error:', error);
+    }
+  };
+
   return (
     <div className="">
       <Whitebg />
       <div className="flex flex-col justify-center items-center max-w-xs mx-auto md:w-full">
         <h1 className={`${me.className} text-black text-xl mt-12 mb-6`}>Contact Us</h1>
         <div className="border border-slate-400 rounded-xl shadow-inner md:w-max drop-shadow-xl bg-[#EDF5F8] bg-opacity-30 overflow-hidden px-4 py-6">
-          <form>
+          <form onSubmit={handleContact}>
             <div className={`${lun.className}`}>
               <div className="flex flex-row">
                 <div className="px-1 mb-2">
-                  <label for="name" className="block text-xs text-black">First Name</label>
-                  <input type="text" id="name" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2 py-0.5" placeholder="First Name" required/>
+                  <label for="name" className="block text-xs text-black">Full Name</label>
+                  <input type="text" id="fullname" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2 py-0.5" placeholder="First Name" required/>
                 </div>
                 <div className="px-1 mb-2">
                   <label for="name" className="block text-xs text-black">Last Name</label>
-                  <input type="text" id="name" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2 py-0.5" placeholder="Surname" required/>
+                  <input type="text" id="lastname" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2 py-0.5" placeholder="Surname" required/>
                 </div>
               </div>
               <div className="md:flex md:flex-row">
@@ -32,7 +54,7 @@ const Contact = () => {
                 </div>
                 <div className="px-1  mb-2">
                     <label for="number" className="block text-xs text-black">Phone Number</label>
-                    <input type="text" id="number" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2 py-0.5" placeholder="Phone Number" required/>
+                    <input type="text" id="phone_number" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2 py-0.5" placeholder="Phone Number" required/>
                 </div>
               </div>
               <div className="px-1 mb-2">
@@ -44,7 +66,7 @@ const Contact = () => {
               </div>
               <div className="px-1 mb-4">
                   <label for="number" className="block text-xs text-black">Message</label>
-                  <textarea type="text" name="" id="desc" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-20 px-2 py-0.5" placeholder="Keterangan Tambahan" required/>
+                  <textarea type="text" name="" id="message" className="bg-gray-50 border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-20 px-2 py-0.5" placeholder="Keterangan Tambahan" required/>
               </div>
               <div className="flex justify-center items-center text-black text-xs mb-2 md:text-base md:mt-6">
                 <input type="radio" name="checkbox" id="Ya"/>
@@ -53,7 +75,7 @@ const Contact = () => {
               <div className="flex justify-center">
                 <button 
                   className={` bg-[#0A3771] px-4 py-2 md:mt-6 md:text-base rounded-lg mt-2 shadow-inner drop-shadow-lg`}
-                  type="button">
+                  type="submit">
                   Submit
                 </button>
               </div>
